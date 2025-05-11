24b4b2dc4a7852a82887d54cf74bde31175dfe4d
Abhishek-Khemnar <khemnarabhishek3@gmail.com>
2025-05-11 22:19:31 +0530

    problem1.5 accept character  from user and check whether that character is vowel or not .c

---
+++ LB Assignment 3/problem1.5 accept character  from user and check whether that character is vowel or not .c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+typedef int BOOL;
+# define TRUE 1
+# define FALSE 0
+BOOL Chkvowel(char cChar)
+{
+    
+    if(cChar=='a'||cChar=='e'||cChar=='i'||cChar=='o'||cChar=='u'||
+    cChar=='A'||cChar=='B'||cChar=='I'||cChar=='O'||cChar=='U')
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+        
+    }
+}
+int main()
+{
+    char cValue ='\0';
+    BOOL bRet = FALSE;
+    printf("Enter character \n");
+    scanf("%c",&cValue);
+    bRet=Chkvowel(cValue);
+    if (bRet==TRUE)
+    {
+        printf("it is vowel");
+    }
+    else
+    {
+        printf("it is not vowel");
+    }
+    return 0;
+}
