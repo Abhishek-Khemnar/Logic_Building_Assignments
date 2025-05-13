150692d5c1a8b82dac7b1c5f95a67c3f1fe67c1f
Abhishek-Khemnar <khemnarabhishek3@gmail.com>
2025-05-13 22:22:08 +0530

    Create problem1.1 accept  number from user and display multiplication of factor.c

---
+++ LB Assignment 4/problem1.1 accept  number from user and display multiplication of factor.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int MultFact(int ino)
+{
+    int Mult=1;
+   for(int iCnt = 1 ; iCnt<=ino/2;iCnt++)
+   {
+     if(ino%iCnt==0)
+     {
+        Mult=Mult*iCnt;
+     }
+   }
+   return Mult;
+}
+int main()
+{
+    int ivalue=0;
+    int iRet =0; 
+    printf("enter number");
+    scanf("%d",&ivalue);
+    iRet=MultFact(ivalue);
+    printf("%d",iRet);
+    return 0;
+}
