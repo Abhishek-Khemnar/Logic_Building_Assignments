244e604cb5136be75f3d48dd6d59f63cd983a344
Abhishek-Khemnar <khemnarabhishek3@gmail.com>
2025-05-13 22:32:39 +0530

    Create problem 1.2 accept number from user and display it's factor in decreasing order.c

---
+++ LB Assignment 4/problem 1.2 accept number from user and display it's factor in decreasing order.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void FactRev(int ino)
+{
+
+   for(int iCnt = ino/2 ; iCnt>=1;iCnt--)
+   {
+     if(ino%iCnt==0)
+     {
+        printf("%d\n",iCnt);
+     }
+   }
+}
+int main()
+{
+    int ivalue=0;
+    printf("enter number");
+    scanf("%d",&ivalue);
+    FactRev(ivalue);
+    return 0;
+}
