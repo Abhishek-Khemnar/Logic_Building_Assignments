fd091ad14b760a07698a7a35d81ca813d17907b8
Abhishek-Khemnar <khemnarabhishek3@gmail.com>
2025-05-15 14:34:09 +0530

    Create problem1.1 accept radius of circle from user and calculate its area.c

---
+++ LB Assignment 8/problem1.1 accept radius of circle from user and calculate its area.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+double CircleArea(float fRadius)
+{
+     double Area = 3.14 * fRadius * fRadius;
+     return Area;
+}
+int main()
+{
+    float fValue = 0.0;
+    float dRet = 0.0;
+    printf("Enter radius");
+    scanf("%f",&fValue);
+    dRet=CircleArea(fValue);
+    printf("Area of circle : %lf",dRet);
+    return  0;
+
+}
