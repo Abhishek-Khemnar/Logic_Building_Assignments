2f1f9ac252265096ae5fb9ff52f7cab1d771a958
Abhishek-Khemnar <khemnarabhishek3@gmail.com>
2025-06-05 15:55:53 +0530

    Create problem1.4 accept number of rows and columns from user and display below pattern.c

---
+++ LB Assignment 13/problem1.4 accept number of rows and columns from user and display below pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void Pattern(int iCol,int iRow)
+{
+    for(int i = iRow; i>0 ; i--)
+    {
+        for(int j = 1; j<=iCol;j++)
+        {
+            printf("%d\t",i);
+        }
+         printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 =0,iValue2 = 0;
+    printf("enter the number of columns");
+    scanf("%d",&iValue1);
+    printf("enter the number of rows");
+    scanf("%d",&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0; 
+}
