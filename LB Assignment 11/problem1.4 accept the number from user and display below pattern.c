62a25ef666f00ed6a65dcb0bffc4f1e26d7c5151
Abhishek-Khemnar <khemnarabhishek3@gmail.com>
2025-06-05 14:22:03 +0530

    Create problem1.4 accept the number from user and display below pattern.c

---
+++ LB Assignment 11/problem1.4 accept the number from user and display below pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void Pattern(int No)
+{
+    
+    for (int iCnt = 1 ; iCnt<=No ; iCnt++)
+    {
+      printf("#\t %d\t *\t",iCnt);
+    }
+   
+}
+int main()
+{
+    int iValue = 0;
+    printf("enter number of elements");
+    scanf("%d",&iValue);
+    Pattern(iValue);
+    return 0;
+}
